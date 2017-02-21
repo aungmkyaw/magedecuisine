0cc613956aa6877fcb4bb82d4afa40459242505a
sukhdipsingh <s_singh26@u.pacific.edu>
2017-02-21 00:33:29 -0800

    Added button action for Recipe Builder

--- app/src/main/java/com/example/amk/magedecuisine/MainActivity.java
+++ app/src/main/java/com/example/amk/magedecuisine/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.amk.magedecuisine;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    public final static String EXTRA_MESSAGE = "com.example.sukhdipsingh.magecuisine.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /** CALLED WHEN USER CLICKS RECIPE BUILDER FUNCTION **/
+    public void doSomething(View view)
+    {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        Button recipeButton = (Button) findViewById(R.id.button16);
+        String message = "Senior Project";
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
