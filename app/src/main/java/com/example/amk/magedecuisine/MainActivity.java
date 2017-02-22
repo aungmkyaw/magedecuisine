2d70fa42573bf3d594190dd5890c22dda0babf7c
sukhdipsingh <s_singh26@u.pacific.edu>
2017-02-22 11:43:04 -0800

    removing logo

--- app/src/main/java/com/example/amk/magedecuisine/MainActivity.java
+++ app/src/main/java/com/example/amk/magedecuisine/MainActivity.java
@@ -7,7 +7,7 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    public final static String EXTRA_MESSAGE = "com.example.sukhdipsingh.magecuisine.MESSAGE";
+    public final static String EXTRA_MESSAGE = "com.example.amk.magedecuisine.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
