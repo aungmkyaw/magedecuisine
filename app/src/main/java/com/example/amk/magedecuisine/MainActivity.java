dea62f931744d6a7ff092dd415cadd09fd928eba
sukhdipsingh <s_singh26@u.pacific.edu>
2017-02-22 13:25:24 -0800

    Adding Pantry Activity

--- app/src/main/java/com/example/amk/magedecuisine/MainActivity.java
+++ app/src/main/java/com/example/amk/magedecuisine/MainActivity.java
@@ -1,8 +1,8 @@
 package com.example.amk.magedecuisine;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
@@ -15,13 +15,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    /** CALLED WHEN USER CLICKS RECIPE BUILDER FUNCTION **/
+    /** CALLED WHEN USER CLICKS RECIPE BUILDER BUTTON **/
     public void doSomething(View view)
     {
         Intent intent = new Intent(this, DisplayMessageActivity.class);
         Button recipeButton = (Button) findViewById(R.id.button16);
         String message = "Senior Project";
         intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+    /** CALLED WHEN USER CLICKS PANTRY BUTTON **/
+    public void pantryAction(View view)
+    {
+        Intent intent = new Intent(this, Pantry.class);
+        Button pantryButton = (Button) findViewById(R.id.button14);
         startActivity(intent);
     }
 }
