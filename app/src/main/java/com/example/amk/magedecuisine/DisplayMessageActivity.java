4f80820f3b1c88896ca4d079cbe2c24419d59aac
sukhdipsingh <s_singh26@u.pacific.edu>
2017-03-05 14:24:03 -0800

    adding unirest jar

--- app/src/main/java/com/example/amk/magedecuisine/DisplayMessageActivity.java
+++ app/src/main/java/com/example/amk/magedecuisine/DisplayMessageActivity.java
@@ -1,10 +1,16 @@
 package com.example.amk.magedecuisine;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+
 
 public class DisplayMessageActivity extends AppCompatActivity {
 
@@ -21,5 +27,17 @@ public class DisplayMessageActivity extends AppCompatActivity {
 
         ViewGroup layout = (ViewGroup) findViewById(R.id.activity_display_message);
         layout.addView(textView);
+    }
+
+    public void APICall()
+    {
+        try {
+            HttpResponse<JsonNode> response = Unirest.get("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?fillIngredients=false&ingredients=apples%2Cflour%2Csugar&limitLicense=false&number=5&ranking=1")
+                    .header("X-Mashape-Key", "KgebgXWQeHmshgowAPA7lmc3utfAp1Vu0jyjsnN2rSrkXexgCY")
+                    .header("Accept", "application/json")
+                    .asJson();
+        } catch (UnirestException e) {
+            e.printStackTrace();
+        }
     }
 }
