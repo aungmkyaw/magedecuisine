355763187d925300408bb2fc0ffe8ffa6c5fb822
sukhdipsingh <s_singh26@u.pacific.edu>
2017-03-17 10:22:36 -0700

    adding the rest of the JSON parser classes

--- app/src/main/java/com/example/amk/magedecuisine/DisplayListView.java
+++ app/src/main/java/com/example/amk/magedecuisine/DisplayListView.java
@@ -2,6 +2,7 @@ package com.example.amk.magedecuisine;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -12,16 +13,34 @@ public class DisplayListView extends AppCompatActivity {
     String json_string;
     JSONObject jsonObject;
     JSONArray jsonArray;
+    RecipesAdapter recipesAdapter;
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.display_listview_layout);
+        listView = (ListView) findViewById(R.id.listview);
+        listView.setAdapter(recipesAdapter);
+        recipesAdapter = new RecipesAdapter(this, R.layout.row_layout);
         json_string = getIntent().getExtras().getString("json_data");
         try {
             jsonObject = new JSONObject(json_string);
+            jsonArray = jsonObject.getJSONArray("server_response");
+            int count = 0;
+            String id, title, likes;
 
-            int count;
+            while(count < jsonArray.length())
+            {
+                JSONObject JO = jsonArray.getJSONObject(count);
+                id = JO.getString("id");
+                title = JO.getString("title");
+                likes = JO.getString("likes");
+
+                Recipes recipe = new Recipes(id, title, likes);
+                recipesAdapter.add(recipe);
+                count++;
+            }
 
         } catch (JSONException e) {
             e.printStackTrace();
--- app/src/main/java/com/example/amk/magedecuisine/RecipesAdapter.java
+++ app/src/main/java/com/example/amk/magedecuisine/RecipesAdapter.java
@@ -1,9 +1,11 @@
 package com.example.amk.magedecuisine;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,29 +21,57 @@ public class RecipesAdapter extends ArrayAdapter{
         super(context, resource);
     }
 
-    @Override
-    public void add(Object object)
+    public void add(Recipes object)
     {
         super.add(object);
+        list.add(object);
     }
 
     @Override
     public int getCount()
     {
-        return super.getCount();
+        return list.size();
     }
 
     @Override
     public Object getItem(int position)
     {
-        return super.getItem(position);
+        return list.get(position);
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent)
     {
-        return super.getView(position, convertView, parent);
+        View row = convertView;
+        RecipeHolder recipeHolder;
+
+        if (row == null)
+        {
+            LayoutInflater layoutInflater = (LayoutInflater) this.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            row = layoutInflater.inflate(R.layout.row_layout, parent, false);
+            recipeHolder = new RecipeHolder();
+            recipeHolder.tx_id = (TextView) row.findViewById(R.id.tx_id);
+            recipeHolder.tx_title = (TextView) row.findViewById(R.id.tx_title);
+            recipeHolder.tx_likes = (TextView) row.findViewById(R.id.tx_likes);
+            row.setTag(recipeHolder);
+        }
+
+        else
+        {
+            recipeHolder = (RecipeHolder) row.getTag();
+        }
+
+        Recipes recipes = (Recipes) this.getItem(position);
+        recipeHolder.tx_id.setText(recipes.getId());
+        recipeHolder.tx_title.setText(recipes.getTitle());
+        recipeHolder.tx_likes.setText(recipes.getLikes());
+
+        return row;
     }
 
+    static class RecipeHolder
+    {
+        TextView tx_id, tx_title, tx_likes;
+    }
 
 }
