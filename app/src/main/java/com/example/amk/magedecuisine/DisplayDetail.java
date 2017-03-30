44d7b98f7b75c8c4a9a5b959bc7846d7d67503af
sukhdipsingh <s_singh26@u.pacific.edu>
2017-03-29 20:09:22 -0700

    adding Recipe Details files

---
+++ app/src/main/java/com/example/amk/magedecuisine/DisplayDetail.java
@@ -0,0 +1,13 @@
+package com.example.amk.magedecuisine;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DisplayDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_detail);
+    }
+}
---
+++ app/src/main/java/com/example/amk/magedecuisine/RecipeDetails.java
@@ -0,0 +1,8 @@
+package com.example.amk.magedecuisine;
+
+/**
+ * Created by sukhdipsingh on 3/29/17.
+ */
+
+public class RecipeDetails {
+}
